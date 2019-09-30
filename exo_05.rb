cc2b1bd3b00ffd5cb883b73382eb704da941abc2
Antoinebousquet <moodlinkproject@gmail.com>
2019-09-30 19:52:22 +0200

    c'est tout pour aujourd'hui

---
+++ exo_05.rb
@@ -0,0 +1,32 @@
+# que fait #{} ?  
+# Il répond au problème d'ordre mathématique contenu dans le tableau hash.
+
+# le programme lit la chaîne de caractère.
+ puts "On va compter le nombre d'heures de travail à THP"
+
+# le programme lit la chaîne de caractère puis résoud l'opération et affiche le résultat.
+puts "Travail : #{10 * 5 * 11}"
+# le programme lit la chaîne de caractère puis résoud l'opération et affiche le résultat.
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# le programme lit la chaîne de caractère.
+puts "Et en secondes ?"
+# Le programme résoud l'opération et affiche le résultat.
+puts 10 * 5 * 11 * 60 * 60
+# le programme lit la chaîne de caractère et sans # ne peut inidiquer si l'opération est jsute ou non.
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Le programme résoud l'opération et affiche le résultat: opération fausse.
+puts 3 + 2 < 5 - 7
+# le programme lit la chaîne de caractère et dans un #résoud l'opération et affiche le résultat.
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# le programme lit la chaîne de caractère et dans un #résoud l'opération et affiche le résultat.
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# le programme lit la chaîne de caractère.
+puts "Ok, c'est faux alors !"
+# le programme lit la chaîne de caractère.
+puts "C'est drôle ça, faisons-en plus :"
+# le programme lit la chaîne de caractère et dans un #résoud l'opération et affiche le résultat.
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# le programme lit la chaîne de caractère et dans un #résoud l'opération et affiche le résultat.
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# le programme lit la chaîne de caractère et dans un #résoud l'opération et affiche le résultat.
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
