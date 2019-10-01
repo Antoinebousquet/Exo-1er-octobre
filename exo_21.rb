e0c95428cd5ac34f1c5173b88ef3690da94b1170
AlisonJardon <alison.jardon@gmail.com>
2019-10-01 18:11:31 +0200

    pyramide inversée en while

---
+++ exo_21.rb
@@ -0,0 +1,17 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+nombre = Integer(gets) rescue false
+
+while nombre == false || nombre < 1 || nombre > 25
+puts "Choisis un nombre entre 1 et 25"
+print "> "
+nombre = Integer(gets) rescue false
+end
+
+i = 1
+puts "Voici la pyramide :"
+nombre.times do 
+ puts "#{" " * (nombre - i) + '#' * i}" 
+	i = i + 1
+
+end 															
