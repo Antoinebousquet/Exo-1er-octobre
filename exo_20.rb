4b913ef3cffc8d353434d3ea003150b3c941c85b
AlisonJardon <alison.jardon@gmail.com>
2019-10-01 17:50:57 +0200

    pyramide avec while

--- exo_20.rb
+++ exo_20.rb
@@ -1,12 +1,17 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
-nombre = gets.chomp.to_i
-puts "Voici la pyramide :"
+nombre = Integer(gets) rescue false
 
-i = "#"
-
-nombre.times do 
- puts i
-	i = i + "#"
-
+while nombre == false || nombre < 1 || nombre > 25
+puts "Choisis un nombre entre 1 et 25"
+print "> "
+nombre = Integer(gets) rescue false
 end
+
+i = 1
+puts "Voici la pyramide :"
+nombre.times do 
+ puts "#" * i
+	i = i + 1
+
+end 
